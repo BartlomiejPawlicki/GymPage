e55ee36fd878d4f19feadad3f5dd100f3f98705a
Bartlomiej Pawlicki <barpawlicki@gmail.com>
2021-03-16 23:51:15 +0100

    Updates

---
+++ precache-manifest.3b3b96cd48a4112e25b953d3200a63f2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e844bfe6345a66fe319",
+    "url": "/GymPage/static/css/main.4de98350.chunk.css"
+  },
+  {
+    "revision": "7e844bfe6345a66fe319",
+    "url": "/GymPage/static/js/main.69f154d4.chunk.js"
+  },
+  {
+    "revision": "22f9b5694c56f036149c",
+    "url": "/GymPage/static/js/runtime~main.0f8bd0f9.js"
+  },
+  {
+    "revision": "679f495286fc314eb154",
+    "url": "/GymPage/static/js/2.14dd36b9.chunk.js"
+  },
+  {
+    "revision": "6059f60420929c193e3fe2a9feae1877",
+    "url": "/GymPage/static/media/gym2.6059f604.jpg"
+  },
+  {
+    "revision": "6a1ada5e7d6c561be4def695818a97a0",
+    "url": "/GymPage/static/media/gym1.6a1ada5e.jpg"
+  },
+  {
+    "revision": "d767b9504578e7b2ff7f80c82244b45a",
+    "url": "/GymPage/static/media/gym4.d767b950.jpg"
+  },
+  {
+    "revision": "c12f79c4e1b043560983d90dc8b337e9",
+    "url": "/GymPage/static/media/product.c12f79c4.jpg"
+  },
+  {
+    "revision": "12ba5215b93f3189881fa741211cfefc",
+    "url": "/GymPage/static/media/machines.12ba5215.jpg"
+  },
+  {
+    "revision": "02e09d5d65c9c6d15cd068725b2bb03f",
+    "url": "/GymPage/static/media/formularz.02e09d5d.png"
+  },
+  {
+    "revision": "fe839e7d135aed927f92c21a7cb2e77c",
+    "url": "/GymPage/static/media/adm.fe839e7d.jpeg"
+  },
+  {
+    "revision": "8d202e4e03cb8f5bc9b99f138ecfa0f9",
+    "url": "/GymPage/static/media/adminpanel2.8d202e4e.jpg"
+  },
+  {
+    "revision": "84d93033c90766f3da03b3ea94ce0a9a",
+    "url": "/GymPage/static/media/logo-gym.84d93033.png"
+  },
+  {
+    "revision": "c24123ccf1592a7dac6635df3233baf7",
+    "url": "/GymPage/static/media/basket.c24123cc.svg"
+  },
+  {
+    "revision": "bcf37c96a5560d36260c2f22b44663c7",
+    "url": "/GymPage/index.html"
+  }
+];
